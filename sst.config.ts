9b098af1eb22b4dafff164f65a2c565685f87ff9
Katsuhisa Ueda <moochannel@gmail.com>
2024-12-09 14:59:48 +0900

    Initialize using create-sst@two

---
+++ sst.config.ts
@@ -0,0 +1,14 @@
+import { SSTConfig } from "sst";
+import { API } from "./stacks/MyStack";
+
+export default {
+  config(_input) {
+    return {
+      name: "todo-sstv2-sveltekit",
+      region: "ap-northeast-1",
+    };
+  },
+  stacks(app) {
+    app.stack(API);
+  }
+} satisfies SSTConfig;
