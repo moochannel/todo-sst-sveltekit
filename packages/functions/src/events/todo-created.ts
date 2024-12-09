de2099168e174176b60ec2cd977ed52d0bfa23ee
Katsuhisa Ueda <moochannel@gmail.com>
2024-12-09 18:26:48 +0900

    Remove sample functions

--- packages/functions/src/events/todo-created.ts
+++
@@ -1,7 +0,0 @@
-import { Todo } from '@todo-sstv2-sveltekit/core/todo'
-import { EventHandler } from 'sst/node/event-bus'
-
-// eslint-disable-next-line @typescript-eslint/require-await
-export const handler = EventHandler(Todo.Events.Created, async (evt) => {
-  console.log('Todo created', evt)
-})
