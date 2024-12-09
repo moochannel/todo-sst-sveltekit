70fb0c2aa35fb3c6e3d3cdd3d32f29a07b601d6a
Katsuhisa Ueda <moochannel@gmail.com>
2024-12-09 16:09:09 +0900

    Add Linter and Formatter

---
+++ eslint.config.js
@@ -0,0 +1,32 @@
+import js from '@eslint/js'
+import eslintConfigPrettier from 'eslint-config-prettier'
+import ts from 'typescript-eslint'
+
+export default ts.config(
+  {
+    ignores: [
+      './**/node_modules/',
+      '.sst/',
+    ],
+  },
+  // JavaScript
+  js.configs.recommended,
+  // TypeScript
+  ...ts.configs.recommendedTypeChecked,
+  // ...tseslint.configs.strictTypeChecked,
+  // ...tseslint.configs.stylisticTypeChecked,
+  {
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+  {
+    files: ["eslint.config.js"],
+    ...ts.configs.disableTypeChecked
+  },
+  // Prettier
+  eslintConfigPrettier,
+)
