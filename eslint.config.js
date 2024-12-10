0ba9e43c6f096ca2484cc21316072cae7372bb2a
Katsuhisa Ueda <moochannel@gmail.com>
2024-12-10 17:56:37 +0900

    Add SvelteKit site into stack

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,20 @@
 import js from '@eslint/js'
-import eslintConfigPrettier from 'eslint-config-prettier'
+import prettier from 'eslint-config-prettier'
+import svelte from 'eslint-plugin-svelte'
+import globals from 'globals'
 import ts from 'typescript-eslint'
+
+import svelteConfig from './packages/frontend/svelte.config.js'
 
 export default ts.config(
   {
-    ignores: ['./**/node_modules/', '.sst/'],
+    ignores: [
+      './**/node_modules/',
+      '.sst/',
+      'packages/frontend/build/',
+      'packages/frontend/.svelte-kit/',
+      'packages/frontend/package/',
+    ],
   },
   // JavaScript
   js.configs.recommended,
@@ -12,18 +22,41 @@ export default ts.config(
   ...ts.configs.recommendedTypeChecked,
   // ...tseslint.configs.strictTypeChecked,
   // ...tseslint.configs.stylisticTypeChecked,
+
+  // Prettier
+  ...svelte.configs['flat/recommended'],
+  prettier,
+  ...svelte.configs['flat/prettier'],
   {
     languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
       parserOptions: {
         projectService: true,
         tsconfigRootDir: import.meta.dirname,
+        extraFileExtensions: ['.svelte'],
       },
     },
   },
   {
-    files: ['eslint.config.js'],
+    files: [
+      'eslint.config.js',
+      'packages/frontend/svelte.config.js',
+      'packages/frontend/tailwind.config.ts',
+      'packages/frontend/postcss.config.js',
+      'packages/frontend/drizzle.config.ts',
+    ],
     ...ts.configs.disableTypeChecked,
   },
-  // Prettier
-  eslintConfigPrettier,
+  {
+    files: ['packages/frontend/**/*.svelte'],
+    languageOptions: {
+      parserOptions: {
+        parser: ts.parser,
+        svelteConfig,
+      },
+    },
+  },
 )
--- stacks/MyStack.ts
+++ stacks/MyStack.ts
@@ -1,4 +1,4 @@
-import { Api, StackContext } from 'sst/constructs'
+import { Api, StackContext, SvelteKitSite } from 'sst/constructs'
 
 export function API({ stack }: StackContext) {
   const api = new Api(stack, 'api', {
@@ -7,7 +7,16 @@ export function API({ stack }: StackContext) {
     },
   })
 
+  const site = new SvelteKitSite(stack, 'site', {
+    path: 'packages/frontend',
+    environment: {
+      DATABASE_URL: process.env.DATABASE_URL || '',
+      BACKEND_BASE_URL: api.url,
+    },
+  })
+
   stack.addOutputs({
+    URL: site.url,
     ApiEndpoint: api.url,
   })
 }
